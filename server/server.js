619642db07a0313bcccb4393580b73b0a9789541
stajud001c <sheik_tajudeen@comcast.com>
2017-03-09 10:35:58 -0700

    Added broadcast emit

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'admin',
+    text:'Welcome to chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'admin',
+    text:'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage',(newMessage)=> {
     console.log(newMessage);
     io.emit('newMessage',{
@@ -22,6 +34,11 @@ io.on('connection',(socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from:newMessage.from,
+    //   text:newMessage.text,
+    //   createdAt:new Date().getTime()
+    // });
   })
   socket.on('disconnect',() => {
     console.log('Disconnected from client');
