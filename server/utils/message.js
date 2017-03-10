9677dd9a11f8f72ebba3be7c77ee29b722ef8901
stajud001c <sheik_tajudeen@comcast.com>
2017-03-09 22:02:50 -0700

    Geo location

--- server/utils/message.js
+++ server/utils/message.js
@@ -3,9 +3,18 @@ var generateMessage = (from,text) => {
     from,
     text,
     createdAt: new Date().getTime()
+  };
+};
+
+var generateLocationMessage = (from, lat, lng) => {
+  return{
+    from,
+    url: `https://www.google.com/maps?q=${lat},${lng}`,
+    createdAt: new Date().getTime()
   }
 };
 
 module.exports = {
-  generateMessage
+  generateMessage,
+  generateLocationMessage
 };
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message.js');
+var {generateMessage,generateLocationMessage} = require('./message.js');
 
 describe('Generate message', () => {
   it('Should generate correct message', () => {
@@ -12,4 +12,17 @@ describe('Generate message', () => {
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from,text});
   });
+});
+
+describe('Generate location message', () => {
+  it('should generate correct location object',() => {
+    var lat = 1;
+    var lng = 1;
+    var url =`https://www.google.com/maps?q=${lat},${lng}`;
+    var locationMessage = generateLocationMessage('Admin',lat,lng);
+
+    expect(locationMessage.createdAt).toBeA('number');
+    expect(locationMessage.url).toBe(url);
+    expect(locationMessage.from).toBe('Admin');
+  })
 });
